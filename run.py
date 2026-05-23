1cddf070687e62cfba1ceaf0cd1c9bf1ae2ab46a
Md Abdul Bari <abdulbarimr10@gmail.com>
2026-05-23 20:58:03 +0530

    Fix Docker models exclusion

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 from app.main import create_app
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
